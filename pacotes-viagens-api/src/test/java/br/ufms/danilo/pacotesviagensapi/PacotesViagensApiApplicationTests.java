821ada75fd11146568c4cd33538f79015e5780f3
DaniloRomano <daniloapromano@live.com>
2022-11-03 22:03:42 -0400

    [FEATURE] Projeto da api, configurado para acesso ao banco de dados.

---
+++ pacotes-viagens-api/src/test/java/br/ufms/danilo/pacotesviagensapi/PacotesViagensApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufms.danilo.pacotesviagensapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PacotesViagensApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
